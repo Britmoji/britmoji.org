48cd0be6afa2cc015f2c04488081c0b53d327a63
Koding Dev <kodingdev@pm.me>
2022-07-03 01:29:57 +1000

    improved

--- src/js/britmoji.js
+++ src/js/britmoji.js
@@ -50,7 +50,13 @@ let dvd = {
 function onClick(e) {
     const x = e.clientX;
     const y = e.clientY;
-    clicks.push({ x: x, y: y, scale: 30, phrase: phrases[Math.floor(Math.random() * phrases.length)], color: randColor() });
+    clicks.push({
+        x: x,
+        y: y,
+        scale: 30,
+        phrase: phrases[Math.floor(Math.random() * phrases.length)],
+        color: randColor()
+    });
 }
 
 function update() {
@@ -129,4 +135,24 @@ function onHit() {
     if (dvd.x + dvd.img.width * scale >= canvas.width) {
         dvd.x = canvas.width - dvd.img.width * scale;
     }
+}
+
+if (window.addEventListener) window.addEventListener('DOMMouseScroll', wheel, false);
+window.onmousewheel = document.onmousewheel = wheel;
+
+function wheel(event) {
+    var delta = 0;
+    if (event.wheelDelta) delta = event.wheelDelta / 120;
+    else if (event.detail) delta = -event.detail / 3;
+    handle(delta);
+    if (event.preventDefault) event.preventDefault();
+    event.returnValue = false;
+}
+
+function handle(delta) {
+    var time = 1000;
+    var distance = 300;
+    $('html, body').stop().animate({
+        scrollTop: $(window).scrollTop() - (distance * delta)
+    }, time);
 }
